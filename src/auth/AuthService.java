fbfb68a011fba5df515842d7466fb06d598f3907
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2026-01-29 21:31:29 -0300

    Refinamento no fluxo de Login
    A classe AuthService foi movida para o pacote auth, e agora esta junta a classe UserSession. O objetivo desta mudança é organizar o pacote auth para funções relacionadas a segurança, enquanto o restante do projeto mantém uma arquiterura em camadas.
    
    O método login foi refatorado, agora apenas uma busca atrás do usuário informado é realzida no banco de dados simulado. foi removido o método existUser

--- src/auth/AuthService.java
+++ src/auth/AuthService.java
@@ -1,9 +1,9 @@
-package service;
+package auth;
 
 import java.util.List;
 
 import model.Credentials;
-
+import model.User;
 import model.enums.EntityStatus;
 import repository.CredentialsDAO;
 import repository.UserDAO;
@@ -13,26 +13,30 @@ public class AuthService {
     private static final UserDAO userDAO = UserDAO.getInstance();
     private static final CredentialsDAO credentialsDAO = CredentialsDAO.getInstance();
 
-    public void login(String email, String password) {
-        if (existUser(email)) {
-            Long userId = userDAO.findByEmail(email).getId();
-            if (validatePassword(userId, password)) {
-                System.out.println("Login Efetuado com sucesso!");
-            } else {
-                System.out.println("Senha incorreta!");
-            }
+    public boolean login(String email, String password) {
 
-        } else {
+        User user = userDAO.findByEmail(email);
+
+        if (user == null) {
             System.out.println("Email não cadastrado");
         }
 
-    }
+        if (user.getStatus() != EntityStatus.ACTIVE) {
+            System.out.println("Usuário está inativo ou bloqueado.");
+            return false;
+        }
 
-    public boolean existUser(String email) {
-        System.out.println(userDAO.findByEmail(email));
-        return userDAO.findByEmail(email) != null;
-    }
+        if (validatePassword(user.getId(), password)) {
+            UserSession.getInstance().startSession(user.getId(), user.getEmail(), user.getUserRole());
+            System.out.println("Login Efetuado com sucesso!");
+            return true;
+        } else {
+            System.out.println("Senha incorreta!");
+            return false;
+        }
 
+    }
+    
     public boolean validatePassword(Long userId, String password) {
         List<Credentials> list = credentialsDAO.findByUserId(userId);
         for (Credentials c : list) {
