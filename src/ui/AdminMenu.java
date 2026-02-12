61d991d4f433d4da0a56a3da669e5b90aad598de
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2026-02-11 21:30:16 -0300

    Iniciando implementação da classe AdminMenu

--- src/ui/AdminMenu.java
+++ src/ui/AdminMenu.java
@@ -4,8 +4,18 @@ public class AdminMenu implements Menu {
 
     @Override
     public void showMenu() {
-        throw new UnsupportedOperationException("Unimplemented method 'showMenu'");
-    }
+    System.out.println("\n####################################");
+    System.out.println("            Painel do ADMIN");
+    System.out.println("1. Meu Perfil");
+    System.out.println("2. Listar Usuários");
+    System.out.println("3. Criar Conta (ADMIN/STOKIST)");
+    System.out.println("4. Ativar / Desativar Usuário");
+    System.out.println("5. Resetar Senha de Usuário");
+    System.out.println("6. Listar STOKIST");
+    System.out.println("7. Bloquear / Desbloquear Usuário");
+    System.out.println("0. Logout");
+    System.out.print("Escolha: ");
+}
 
     @Override
     public void setPath() {
